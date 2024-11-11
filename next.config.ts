561d6842e63a7aaf20780ad9865b158dd84876a9
Richard Shaju <richardshaju66@gmail.com>
2024-11-11 15:24:18 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const path = require('path');
+const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        hostname: "firebasestorage.googleapis.com",
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
       },
     ],
+    domains: ["localhost", "*.googleusercontent.com", "drive.google.com", "lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
   },
+  reactStrictMode: false,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
